7884dd08ee3fd6baf3068a5b9ef6fad0fd80ecdb
ayush18pop <ayushsp05@gmail.com>
2025-03-31 00:14:52 +0530

    updated env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,15 +10,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyASW_JbOfpJ2DkCmzO7AOlkjv806IGUnUg",
-  authDomain: "lnmdoubtsolverz.firebaseapp.com",
-  projectId: "lnmdoubtsolverz",
-  storageBucket: "lnmdoubtsolverz.firebasestorage.app",
-  messagingSenderId: "289840486336",
-  appId: "1:289840486336:web:04f0db519c3278538a5a25",
-  measurementId: "G-PHCM0327KB",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
+console.log(import.meta.env.VITE_FIREBASE_PROJECT_ID);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
